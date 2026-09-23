51a70d70b94c4dcc3d0600e7655ece206dd738f4
chiichiimoo1991-debug <chiichiimoo1991@gmail.com>
2026-09-23 11:14:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const cards = \[\];\
 \ `frontInput.value = "";
 \ `backInput.value = "";
 }
+const card = [];\
+\ `function addCard, front, back, {
+\ `cards.push({front: front, back: back});
+}
+form.addEventListener('submit', (e) => { e.preventDefault(); });
+{e.preventDefault
+const front = document.getElementById("frontInput").value;
+const back = document.getElementById("backInput").value; \ `cards.push({front: front, back: back});
+\ `frontInput.value = "";
+backInput.value = "";
